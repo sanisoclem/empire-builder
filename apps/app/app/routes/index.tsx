8f2638873ae10c5fee1c6fe043f91b40b99fc1de
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2023-03-04 19:08:25 +1100

    prisma and db migrations

--- apps/app/app/routes/index.tsx
+++ apps/app/app/routes/index.tsx
@@ -1,16 +1,31 @@
+import { LoaderFunction } from "@remix-run/server-runtime";
 import { Button } from "ui";
+import { PrismaClient } from "db-totality";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async (args) => {
+  const client = new PrismaClient();
+  try {
+    const users = await client.user.findMany();
+    return users;
+  } catch (ex) {
+    console.log(ex);
+  } finally {
+    client.$disconnect();
+  }
+};
 
 export default function Index() {
+  const loaderData = useLoaderData();
   return (
     <div className="container">
+      {JSON.stringify(loaderData)}
       <h1 className="title">
         Blog <br />
         <span>Kitchen Sink</span>
       </h1>
       <Button />
-      <p className="description">
-        Built With{" "}
-      </p>
+      <p className="description">Built With </p>
     </div>
   );
 }
