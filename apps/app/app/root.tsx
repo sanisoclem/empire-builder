0b83cd311d222dd5f43a221bd1946adb064f6fd9
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2023-03-04 23:17:51 +1100

    cleanup stytles

--- apps/app/app/root.tsx
+++ apps/app/app/root.tsx
@@ -1,42 +1,38 @@
-import type {
-  MetaFunction,
-  LoaderFunction,
-} from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
-import { rootAuthLoader } from "@clerk/remix/ssr.server";
-import styles from "~/tailwind.css";
-import { Analytics } from "@vercel/analytics/react";
-import { useTheme } from "~hooks";
-import { Modals } from "~components";
+import type { MetaFunction, LoaderFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { ClerkApp, ClerkCatchBoundary } from '@clerk/remix';
+import { rootAuthLoader } from '@clerk/remix/ssr.server';
+import styles from '~/tailwind.css';
+import { Analytics } from '@vercel/analytics/react';
+import { useTheme } from '~hooks';
+import { Modals } from '~components';
 
 export const loader: LoaderFunction = (args) => {
   return rootAuthLoader(args);
 };
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Empire Builder",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Empire Builder',
+  viewport: 'width=device-width,initial-scale=1'
 });
 
 export const CatchBoundary = ClerkCatchBoundary();
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: 'stylesheet', href: styles },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Yaldevi'
+    }
+  ];
 }
 
 function App() {
   const { theme } = useTheme();
   return (
-    <html lang="en" className={`h-full ${theme === "dark" ? "dark" : ""}`}>
+    <html lang="en" className={`h-full ${theme === 'dark' ? 'dark' : ''}`}>
       <head>
         <Meta />
         <Links />
@@ -48,7 +44,7 @@ function App() {
             } else {
               document.documentElement.classList.remove('dark')
             }
-          `,
+          `
           }}
         ></script>
       </head>
