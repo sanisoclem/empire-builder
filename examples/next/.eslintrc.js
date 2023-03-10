7bef19a9f9e823d771a23ed2ff40c61a79e52514
Joelle Ortiz <contact@joelleortiz.me>
2023-03-10 18:53:24 +1100

    eslint test

--- examples/next/.eslintrc.js
+++ examples/next/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['custom']
+  extends: ['custom'],
+  plugins: ['eslint-plugin-react']
 };
--- examples/next/src/pages/_app.tsx
+++ examples/next/src/pages/_app.tsx
@@ -2,6 +2,6 @@ import type { AppProps } from 'next/app';
 import '../styles.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  var tae = 'kekekeke';
+  const tae = 'kekekeke';
   return <Component {...pageProps} />;
 }
