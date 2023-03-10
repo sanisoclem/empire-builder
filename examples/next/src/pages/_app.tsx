f94162b260ac3c85edec02c3015638f0838a77bd
Joelle Ortiz <contact@joelleortiz.me>
2023-03-10 18:58:43 +1100

    eslint test

--- examples/next/src/pages/_app.tsx
+++ examples/next/src/pages/_app.tsx
@@ -2,6 +2,5 @@ import type { AppProps } from 'next/app';
 import '../styles.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const tae = 'kekekeke';
   return <Component {...pageProps} />;
 }
