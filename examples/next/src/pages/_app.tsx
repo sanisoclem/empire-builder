eaacc132482c884f40cd71bcd2bd6bcd4de0322a
Joelle Ortiz <contact@joelleortiz.me>
2023-03-10 18:47:24 +1100

    eslint test

--- examples/next/src/pages/_app.tsx
+++ examples/next/src/pages/_app.tsx
@@ -2,6 +2,6 @@ import type { AppProps } from 'next/app';
 import '../styles.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // testing lint here
+  var tae = 'kekekeke';
   return <Component {...pageProps} />;
 }
